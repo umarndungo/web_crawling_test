4e6da9846d0aea2dea0e0b5688ff1fef06ddb0c7
umarndungo <omarnjagi@gmail.com>
2025-11-10 18:10:30 +0300

    Handling pagination: Crawling through the pages to get all boo items.

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -13,3 +13,23 @@ class BookSpider(scrapy.Spider):
             item["title"] = book.css("h3 > a::attr(title)").get()
             item["price"] = book.css(".price_color::text").get()
             yield item
+
+        # Handling pagination: Crawling through the pages
+        next_page = response.css("li.next > a::attr(href)").get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+        '''
+        TODO: Figure out how to handle pagination by using the book item url, 
+            to gather the following data:        
+                • Name of the book 
+                • Description of the book 
+                • Book category 
+                • Book prices (Including and Excluding taxes) 
+                • Availability
+
+                • Number of reviews 
+                • The image URL of the book cover 
+                • Rating of the book
+        '''
